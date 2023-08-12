a46d67ea694c6676e916a4a55ebb1e3e41dd99c0
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-08-12 16:15:20 +0530

    finish

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -16,6 +16,22 @@ import { Server } from 'socket.io';
 export class GameGateway implements OnModuleInit {
   @WebSocketServer()
   server: Server;
+  gameSession: {
+    [id: string]: {
+      name: string;
+      inProgress: boolean;
+      players: {
+        [id: string]: {
+          name?: string;
+          score?: number;
+        };
+      };
+    };
+  };
+
+  constructor() {
+    this.gameSession = {};
+  }
 
   onModuleInit() {
     this.server.on('connection', (socket) => {
@@ -23,19 +39,77 @@ export class GameGateway implements OnModuleInit {
     });
   }
 
-  @SubscribeMessage('gameSession')
-  onNewGameSession(@MessageBody() body: any) {
-    console.log('Body', body);
-    this.server.emit(body.channel, {
-      msg: body.message,
+  private getPlayers(gameId: string) {
+    return Object.keys(this.gameSession[gameId].players).map((key) => {
+      return {
+        id: key,
+        name: this.gameSession[gameId].players[key].name,
+        score: this.gameSession[gameId].players[key].score,
+      };
     });
   }
 
-  @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any) {
-    console.log('Body', body);
-    this.server.emit('newMessage', {
-      hello: 'taranjeetsingh',
-    });
+  @SubscribeMessage('createSession')
+  onNewGameSession(@MessageBody() body: { gameId: string; name: string }) {
+    const { gameId, name = '' } = body;
+    this.gameSession[gameId] = {
+      name,
+      players: {},
+      inProgress: false,
+    };
+    console.log('created a new session', this.gameSession[gameId]);
+  }
+
+  @SubscribeMessage('playerJoined')
+  onplayerJoin(
+    @MessageBody() body: { gameId: string; name: string; playerId: string },
+  ) {
+    const { gameId, name, playerId } = body;
+    console.log('new request for player to join');
+    // Check if this player already exist
+    const exist = this.gameSession[gameId];
+    if (exist) {
+      this.gameSession[gameId].players[playerId] = {
+        name: name || '',
+        score: 0,
+      };
+      // send a message to the session that player has joined
+      this.server.emit(gameId, {
+        type: 'PlayerjoinedMsg',
+        players: this.getPlayers(gameId),
+      });
+    }
+  }
+
+  @SubscribeMessage('gameStarted')
+  onGameStart(@MessageBody() body: { gameId: string }) {
+    const { gameId } = body;
+    console.log(`${gameId} Game session has been started`);
+    // Check if this player already exist
+    if (this.gameSession[gameId]) {
+      this.gameSession[gameId].inProgress = true;
+      // send a message to the session that player has joined
+      this.server.emit(gameId, {
+        type: 'GameStartedMsg',
+        gameId,
+      });
+    }
+  }
+
+  @SubscribeMessage('gameScored')
+  onGameScore(
+    @MessageBody() body: { gameId: string; playerId: string; score: number },
+  ) {
+    const { gameId, playerId, score } = body;
+    console.log(`${body.gameId} PlayerId has scored`);
+
+    if (this.gameSession[gameId] && this.gameSession[gameId].inProgress) {
+      this.gameSession[gameId].players[playerId].score = score;
+
+      this.server.emit(gameId, {
+        type: 'GameScoredMsg',
+        players: this.getPlayers(gameId),
+      });
+    }
   }
 }
