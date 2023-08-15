9275c85006d82f18266f1bb1a6fabf83035b7384
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-08-15 20:23:50 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { appConfig } from 'app.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: appConfig().url,
+    origin: `${appConfig().url}*`,
   });
   app.useGlobalPipes(
     new ValidationPipe({
