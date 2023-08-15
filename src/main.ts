d5194b2575e1dc0f704015ee948ca97678e4d1a1
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-08-15 20:42:48 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { appConfig } from 'app.config';
+// import { appConfig } from 'app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: appConfig().url,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  });
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
