74c0b95df73092710662c3a3cc94a0a09b86abd1
Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>
2024-03-06 11:19:46 +1100

    Changed region to us-east-1

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,12 +5,11 @@ import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as logs from "aws-cdk-lib/aws-logs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
-import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import * as targets from "aws-cdk-lib/aws-route53-targets";
 
-const prefix = "BoloonsApi";
+const prefix = "BoloonsApiServer";
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -102,7 +101,7 @@ export class CdkStack extends cdk.Stack {
     // Create a record that points to the load balancer
     new route53.ARecord(this, "AliasRecord", {
       zone: zone,
-      recordName: "server.boloons.com",
+      recordName: "api.boloons.com",
       target: route53.RecordTarget.fromAlias(
         new targets.LoadBalancerTarget(service.loadBalancer)
       ),
