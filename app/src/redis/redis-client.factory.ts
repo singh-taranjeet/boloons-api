c242fad9acc7c9921a08760be5f540cf0dbdb65e
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-11-16 17:53:27 -0800

    Added configuration update (#6)
    * Updated cpu required
    
    * Update infra.yml
    
    * Update cdk-stack.ts
    
    * temp
    
    * removed-mongo-uri-in-code
    
    * Update cdk-stack.ts
    
    * Update redis.service.ts
    
    * game-created-by-added
    
    * Update cdk-stack.ts
    
    * Update cdk-stack.ts
    
    * Update cdk-stack.ts
    
    * removed port 6379
    
    * optimised-secrets
    
    * Update cdk-stack.ts
    
    * Update configuration.ts
    
    * added redis password
    
    * saving
    
    * Fixed
    
    * Update cdk-stack.ts
    
    ---------
    
    Co-authored-by: Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>

--- app/src/redis/redis-client.factory.ts
+++ app/src/redis/redis-client.factory.ts
@@ -1,18 +1,14 @@
 import { FactoryProvider } from '@nestjs/common';
 import { createClient } from 'redis';
 import { RedisClient, REDIS_CLIENT } from './redis-client.type';
-import { Secret } from '../utils/configuration';
 
 export const redisClientFactory: FactoryProvider<Promise<RedisClient>> = {
   provide: REDIS_CLIENT,
   useFactory: async () => {
-    const sercret = new Secret();
-    const REDIS_URL = await sercret.getSecretValue('redis-url');
-    const REDIS_PORT = await sercret.getSecretValue('redis-port');
     const client = createClient({
       socket: {
-        host: REDIS_URL,
-        port: REDIS_PORT ? parseInt(REDIS_PORT) : 6379,
+        host: process.env.REDIS_URL,
+        port: 6379,
       },
     });
     await client.connect();
--- app/src/utils/configuration.ts
+++ app/src/utils/configuration.ts
@@ -1,112 +1,102 @@
 import 'dotenv/config';
-import {
-  SecretsManagerClient,
-  GetSecretValueCommand,
-} from '@aws-sdk/client-secrets-manager';
+// import {
+//   SecretsManagerClient,
+//   GetSecretValueCommand,
+// } from '@aws-sdk/client-secrets-manager';
 
-interface SecretType {
-  'mongo-db-url': string;
-  'redis-url': string;
-  'redis-password': string;
-  'redis-port': string;
-}
+// interface SecretType {
+//   'mongo-db-url': string;
+//   'redis-url': string;
+//   'redis-password': string;
+//   'redis-port': string;
+// }
 
-const cacheSecret = memoSecret();
+// const cacheSecret = memoSecret();
 
-function memoCallBack(callBack: () => Promise<SecretType>) {
-  let result;
-  let loading = false;
-  return async () => {
-    if (result) {
-      return result;
-    }
-    if (loading) {
-      function wait() {
-        if (loading) {
-          return new Promise((resolve) => {
-            setTimeout(() => {
-              resolve(wait());
-            }, 1000);
-          });
-        } else {
-          return result;
-        }
-      }
-      return await wait();
-    }
-    loading = true;
-    result = await callBack();
-    loading = false;
+// function memoCallBack(callBack: () => Promise<SecretType>) {
+//   let result;
+//   let loading = false;
+//   return async () => {
+//     if (result) {
+//       return result;
+//     }
+//     if (loading) {
+//       function wait() {
+//         if (loading) {
+//           return new Promise((resolve) => {
+//             setTimeout(() => {
+//               resolve(wait());
+//             }, 1000);
+//           });
+//         } else {
+//           return result;
+//         }
+//       }
+//       return await wait();
+//     }
+//     loading = true;
+//     result = await callBack();
+//     loading = false;
 
-    return result;
-  };
-}
+//     return result;
+//   };
+// }
 
-const fetchSecret = memoCallBack(async () => {
-  const client = new SecretsManagerClient({
-    region: 'us-east-1',
-  });
-  const secretName = 'boloons-api-secret';
-  try {
-    console.log('Making request for secrets');
-    const response = await client.send(
-      new GetSecretValueCommand({
-        SecretId: secretName,
-        VersionStage: 'AWSCURRENT', // VersionStage defaults to AWSCURRENT if unspecified
-      }),
-    );
+// const fetchSecret = memoCallBack(async () => {
+//   if (process.env.NODE_ENV === 'development') {
+//     return {
+//       'mongo-db-url': process.env.DB_URL,
+//       'redis-url': process.env.REDIS_URL,
+//       'redis-password': process.env.REDIS_PASSWORD,
+//       'redis-port': process.env.REDIS_PORT,
+//     };
+//   }
+//   const client = new SecretsManagerClient({
+//     region: 'us-east-1',
+//   });
+//   const secretName = 'prod/boloons/config';
+//   try {
+//     console.log('Making request for secrets');
+//     const response = await client.send(
+//       new GetSecretValueCommand({
+//         SecretId: secretName,
+//         VersionStage: 'AWSCURRENT', // VersionStage defaults to AWSCURRENT if unspecified
+//       }),
+//     );
 
-    return JSON.parse(response.SecretString) as SecretType;
-  } catch (error) {
-    throw error;
-  }
-});
+//     return JSON.parse(response.SecretString) as SecretType;
+//   } catch (error) {
+//     throw error;
+//   }
+// });
 
-function memoSecret() {
-  let cache: SecretType;
+// function memoSecret() {
+//   let cache: SecretType;
 
-  return async (n: keyof SecretType) => {
-    if (cache && cache[n]) {
-      return cache[n];
-    } else {
-      cache = await fetchSecret();
-      return cache[n];
-    }
-  };
-}
+//   return async (n: keyof SecretType) => {
+//     if (cache && cache[n]) {
+//       return cache[n];
+//     } else {
+//       cache = await fetchSecret();
+//       return cache[n];
+//     }
+//   };
+// }
 
-export class Secret {
-  async getSecretValue(n: keyof SecretType) {
-    return cacheSecret(n);
-  }
+// export class Secret {
+//   async getSecretValue(n: keyof SecretType) {
+//     return cacheSecret(n);
+//   }
 
-  private async fetchSecret() {
-    return fetchSecret();
-  }
-}
+// }
+
 
 export default async () => {
-  if (process.env.NODE_ENV === 'developmednt') {
-    return {
-      DB_URL: process.env.DB_URL,
-      REDIS_URL: process.env.REDIS_URL,
-      REDIS_PASSWORD: process.env.REDIS_PASSWORD,
-      REDIS_PORT: process.env.REDIS_PORT,
-    };
-  }
-
-  const secret = new Secret();
-
-  //const memo = await secret.getSecretValue();
-  const DB_URL = await secret.getSecretValue('mongo-db-url');
-  const REDIS_URL = await secret.getSecretValue('redis-url');
-  const REDIS_PASSWORD = await secret.getSecretValue('redis-password');
-  const REDIS_PORT = await secret.getSecretValue('redis-port');
-
   return {
-    DB_URL,
-    REDIS_URL,
-    REDIS_PASSWORD,
-    REDIS_PORT,
-  };
+
+    REDIS_URL: 'localhost',
+    REDIS_PASSWORD: '',
+    REDIS_PORT: '6379',
+    DB_URL: 'localhost:27017',
+  }
 };
